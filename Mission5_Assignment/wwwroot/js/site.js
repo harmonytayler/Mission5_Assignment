931b24e48bc1bf5cb5b5e3438147a32aa17563e1
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-02-05 18:36:54 -0700

    Add project files.

---
+++ Mission5_Assignment/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$("#formSubmit").click(function () {
+    var hours = parseFloat($("#hours").val());
+    if (hours <= 0) {
+        alert("Please enter a positive number.");
+    }
+    else {
+        var finalPrice = $("#hours").val() * $("#price").val();
+        $("#output").text("Total Price: $" + finalPrice.toFixed(2));
+        $("#output").addClass("visible");
+    }
+})
